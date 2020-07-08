b721f87addb3e84178ffc71640e7e5e412d1e5e9
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-07-07 19:13:16 -0700

    Ready for hardware commands

---
+++ public/javascripts/controller.js
@@ -0,0 +1,68 @@
+
+let isOn = false;
+let PWD_VALUE = 0;
+let PWD_AMOUNT = 100;
+
+// On/Off value
+function setStatus() {
+    
+}
+
+function refreshStatus() {
+    $('#status').empty()
+    if(isOn) {
+        $('#status').append('ON')
+    } else {
+        $('#status').append('OFF')
+    }
+}
+
+// PWD value
+function setPWD() {
+
+}
+
+function refreshPWD() {
+    $('#speed').empty()
+    $('#speed').append(PWD_VALUE)
+}
+
+// ON
+document.querySelector('#on').addEventListener('click', event => {
+    // console.log('ON')
+    isOn = true
+    setStatus()
+    refreshStatus()
+});
+
+// OFF
+document.querySelector('#off').addEventListener('click', event => {
+    // console.log('OFF')
+    isOn = false
+    setStatus()
+    refreshStatus()
+});
+
+// UP
+document.querySelector('#up').addEventListener('click', event => {
+    // console.log('UP')
+    PWD_VALUE += PWD_AMOUNT
+    setPWD()
+    refreshPWD()
+});
+
+// DOWN
+document.querySelector('#down').addEventListener('click', event => {
+    // console.log('DOWN')
+    PWD_VALUE -= PWD_AMOUNT
+    setPWD()
+    refreshPWD()
+});
+
+refreshPWD()
+refreshStatus()
+
+
+
+
+
