f56eb2100f2fc7628ad5795184f9fe7273bef471
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-07-10 23:55:45 -0700

    Put LED test in seperate function

--- pi/pi.js
+++ pi/pi.js
@@ -1,32 +1,70 @@
 /* THIS IS WHERE HARDWARE CODE BELONGS */
+var rpio = require('rpio');
 
 let isOn = false;
 let PWD_VALUE = 0;
 let PWD_AMOUNT = 100;
 
 function setOn() {
-    isOn = true
+
+  isOn = true
 };
 
 function setOff() {
-    isOn = false
+  isOn = false
 };
 
 function setDown() {
-    PWD_VALUE -= PWD_AMOUNT
+  PWD_VALUE -= PWD_AMOUNT
 };
 
 function setUp() {
-    PWD_VALUE += PWD_AMOUNT
+  PWD_VALUE += PWD_AMOUNT
 };
 
 function getStatus() {
-    return isOn
+  return isOn
 };
 
 function getValue() {
-    return PWD_VALUE
+  return PWD_VALUE
 };
+
+function test() {
+  var pin = 12;           /* P12/GPIO18 */
+  var range = 1024;       /* LEDs can quickly hit max brightness, so only use */
+  var max = 128;          /*   the bottom 8th of a larger scale */
+  var clockdiv = 8;       /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
+  var interval = 5;       /* setInterval timer, speed of pulses */
+  var times = 5;          /* How many times to pulse before exiting */
+  
+  /*
+  * Enable PWM on the chosen pin and set the clock and range.
+  */
+  rpio.open(pin, rpio.PWM);
+  rpio.pwmSetClockDivider(clockdiv);
+  rpio.pwmSetRange(pin, range);
+  
+  /*
+  * Repeatedly pulse from low to high and back again until times runs out.
+  */
+  var direction = 1;
+  var data = 0;
+  var pulse = setInterval(function() {
+    rpio.pwmSetData(pin, data);
+    if (data === 0) {
+      direction = 1;
+      if (times-- === 0) {
+        clearInterval(pulse);
+        rpio.open(pin, rpio.INPUT);
+        return;
+      }
+    } else if (data === max) {
+      direction = -1;
+    }
+    data += direction;
+  }, interval, data, direction, times);
+}
 
 module.exports = {
     setOn,
@@ -34,5 +72,6 @@ module.exports = {
     setDown,
     setUp,
     getStatus,
-    getValue
+    getValue,
+    test
 }
