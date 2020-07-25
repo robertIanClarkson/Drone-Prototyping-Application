27cdc07ee7010c4bc7143625e8eb54a6b2af726d
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-07-25 14:39:43 -0700

    restore byte reads + debugging read data

--- pi/compass.js
+++ pi/compass.js
@@ -9,20 +9,22 @@ class Compass {
         this.OPTION_0 = 0x24; // Magnetic high resolution, o/p data rate 50 Hz
         this.OPTION_1 = 0x25; // Magnetic full scale selection, +/- 12 gauss
         this.OPTION_2 = 0X26; // Normal mode, magnetic continuous conversion mode
-        
-        this.VALUE_0 = 0x70;
-        this.VALUE_1 = 0x00;
-        this.VALUE_2 = 0X00;
+        // this.VALUE_0 = 0x70;
+        // this.VALUE_1 = 0x60;
+        // this.VALUE_2 = 0X00;
         
         //Default
-        // this.VALUE_0 = 0x18;
-        // this.VALUE_1 = 0x20;
-        // this.VALUE_2 = 0X02;
+        this.VALUE_0 = 0x18;
+        this.VALUE_1 = 0x20;
+        this.VALUE_2 = 0X02;
 
         /* read */
         this.READ_0 = 0x08;
-        this.READ_1 = 0x0A;
-        this.READ_2 = 0x0C;
+        this.READ_1 = 0x09;
+        this.READ_2 = 0x0A;
+        this.READ_3 = 0x0B;
+        this.READ_4 = 0x0C;
+        this.READ_5 = 0x0D;
 
         /* results */
         this.x_axis;
@@ -66,16 +68,25 @@ class Compass {
             i2c.openPromisified(1)
             .then(sensor => {
                 Promise.all([
-                    sensor.readWord(this.SLAVE_ADDRESS, this.READ_0),
-                    sensor.readWord(this.SLAVE_ADDRESS, this.READ_1),
-                    sensor.readWord(this.SLAVE_ADDRESS, this.READ_2),
+                    sensor.readByte(this.SLAVE_ADDRESS, this.READ_0),
+                    sensor.readByte(this.SLAVE_ADDRESS, this.READ_1),
+                    sensor.readByte(this.SLAVE_ADDRESS, this.READ_2),
+                    sensor.readByte(this.SLAVE_ADDRESS, this.READ_3),
+                    sensor.readByte(this.SLAVE_ADDRESS, this.READ_4),
+                    sensor.readByte(this.SLAVE_ADDRESS, this.READ_5)
                 ])
-                .then(([x, y, z]) => {
+                .then(([a, b, c, d, e, f]) => {
+                    console.log(`X_l --> ${a}`)
+                    console.log(`X_h --> ${b}`)
+                    console.log(`Y_l --> ${c}`)
+                    console.log(`Y_h --> ${d}`)
+                    console.log(`Z_l --> ${e}`)
+                    console.log(`Z_h --> ${f}`)
                     sensor.close()
-                    // this.x_axis = this.convert(a, b)
-                    // this.y_axis = this.convert(c, d)
-                    // this.z_axis = this.convert(e, f)
-		            resolve([x, y, z])
+                    this.x_axis = this.convert(a, b)
+                    this.y_axis = this.convert(c, d)
+                    this.z_axis = this.convert(e, f)
+		            resolve([this.x_axis, this.y_axis, this.z_axis])
                 })
                 .catch(err => {
                     sensor.close()
