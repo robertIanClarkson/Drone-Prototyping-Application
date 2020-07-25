b28117377a5e52b9445dce840bddb6dd2507fe72
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-07-25 14:26:20 -0700

    Trying to read word instead of byte

--- pi/compass.js
+++ pi/compass.js
@@ -20,9 +20,9 @@ class Compass {
         // this.VALUE_2 = 0X02;
 
         /* read */
-        this.READ_CMD = 0x08;
-        this.READ_LENGTH = 6;
-        this.READ_BUFFER = Buffer.alloc(6);
+        this.READ_0 = 0x08;
+        this.READ_1 = 0x0A;
+        this.READ_2 = 0x0C;
 
         /* results */
         this.x_axis;
@@ -62,59 +62,32 @@ class Compass {
     }
 
     read() {
-        return new Promise( (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             i2c.openPromisified(1)
-            .then( sensor => {
-                sensor.readI2cBlock(
-                    this.SLAVE_ADDRESS, 
-                    this.READ_CMD,
-                    this.READ_LENGTH,
-                    this.READ_BUFFER,
-                )
-                .then( () => {
-                    console.log(this.READ_BUFFER)
-                    resolve(this.READ_BUFFER)
+            .then(sensor => {
+                Promise.all([
+                    sensor.readWord(this.SLAVE_ADDRESS, this.READ_0),
+                    sensor.readWord(this.SLAVE_ADDRESS, this.READ_1),
+                    sensor.readWord(this.SLAVE_ADDRESS, this.READ_2),
+                ])
+                .then(([x, y, z]) => {
+                    sensor.close()
+                    // this.x_axis = this.convert(a, b)
+                    // this.y_axis = this.convert(c, d)
+                    // this.z_axis = this.convert(e, f)
+		            resolve([x, y, z])
                 })
-                .catch( err => {
-                    reject(err)
+                .catch(err => {
+                    sensor.close()
+                    reject("*** Error reading compass data")
                 })
             })
-            .catch( err => {
-                reject(err)
+            .catch(err => {
+                sensor.close()
+                reject("*** Error opening i2c bus")
             })
         })
     }
-
-    // read() {
-    //     return new Promise((resolve, reject) => {
-    //         i2c.openPromisified(1)
-    //         .then(sensor => {
-    //             Promise.all([
-    //                 sensor.readByte(this.SLAVE_ADDRESS, this.READ_0),
-    //                 sensor.readByte(this.SLAVE_ADDRESS, this.READ_1),
-    //                 sensor.readByte(this.SLAVE_ADDRESS, this.READ_2),
-    //                 sensor.readByte(this.SLAVE_ADDRESS, this.READ_3),
-    //                 sensor.readByte(this.SLAVE_ADDRESS, this.READ_4),
-    //                 sensor.readByte(this.SLAVE_ADDRESS, this.READ_5)
-    //             ])
-    //             .then(([a, b, c, d, e, f]) => {
-    //                 sensor.close()
-    //                 this.x_axis = this.convert(a, b)
-    //                 this.y_axis = this.convert(c, d)
-    //                 this.z_axis = this.convert(e, f)
-	// 	            resolve([this.x_axis, this.y_axis, this.z_axis])
-    //             })
-    //             .catch(err => {
-    //                 sensor.close()
-    //                 reject("*** Error reading compass data")
-    //             })
-    //         })
-    //         .catch(err => {
-    //             sensor.close()
-    //             reject("*** Error opening i2c bus")
-    //         })
-    //     })
-    // }
 }
 
 module.exports = Compass;
