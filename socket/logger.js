0c8745123f8548304f563c76766037c445c1e13e
robertIanClarkson <robert.ian.clarkson@gmail.com>
2020-08-17 02:47:45 -0700

    logger working but basic

--- socket/logger.js
+++ socket/logger.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
-var compassLogLocation = "./../pi/logs/compassLog.txt"
+var compassLogLocation = "/home/pi/Desktop/Cube-Interface/pi/logs/compassLog.txt"
 
 function logCompass(x, y, z) {
-  fs.appendFile(compassLogLocation, `${x} ${y} ${z}`, function (err) {
+  fs.appendFile(compassLogLocation, `${x} ${y} ${z}\n`, function (err) {
     if (err) throw err;
   })
 }
