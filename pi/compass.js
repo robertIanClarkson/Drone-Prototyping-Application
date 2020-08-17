ad955c4eecfe5bf0eabe3fe6dda5ea3712639edc
robertIanClarkson <robert.ian.clarkson@gmail.com>
2020-08-16 20:41:23 -0700

    Compass working. adding tilt compensation

--- pi/compass.js
+++ pi/compass.js
@@ -11,7 +11,7 @@ class Compass {
     this.OPTION_2 = 0x02; // Normal mode, magnetic continuous conversion mode
     
     this.VALUE_0 = 0x98;
-    this.VALUE_1 = 0x20;
+    this.VALUE_1 = 0xE0;
     this.VALUE_2 = 0x00;
 
     /* read */
@@ -103,9 +103,9 @@ class Compass {
       this.recursiveRead(sensor)
         .then(() => {
           this.i = 0;
-          this.x_axis = (this.x_axis / this.bufferSize) / 1100
-          this.y_axis = (this.y_axis / this.bufferSize) / 1100
-          this.z_axis = (this.z_axis / this.bufferSize) / 980
+          this.x_axis = (this.x_axis / this.bufferSize)
+          this.y_axis = (this.y_axis / this.bufferSize)
+          this.z_axis = (this.z_axis / this.bufferSize)
           this.x_axis_f = Math.round((1.0 - this.filter) * this.x_axis_f + this.filter * this.x_axis)
           this.y_axis_f = Math.round((1.0 - this.filter) * this.y_axis_f + this.filter * this.y_axis)
           this.z_axis_f = Math.round((1.0 - this.filter) * this.z_axis_f + this.filter * this.z_axis)
