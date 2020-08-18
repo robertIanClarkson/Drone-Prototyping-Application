df4018f1bce1c338464ab96ad0af0452ffce1779
Robert Clarkson <robert.ian.clarkson@gmail.com>
2020-08-17 19:09:16 -0700

    Added face colors

--- public/javascripts/cube_animation.js
+++ public/javascripts/cube_animation.js
@@ -7,66 +7,70 @@ class CubeAnimation {
 
   init() {
     var container = document.getElementById('cube-anime');
-  
+
     // scene - main object
     var scene = new THREE.Scene();
     scene.background = new THREE.Color(0xffffff);
-    
+
     // camera - where you are viewing from
     var camera = new THREE.PerspectiveCamera(50, container.clientWidth / container.clientWidth, 1, 2000); // see THREE doc
     camera.position.set(5, 5, 5); // location of camera relative to origin - xyz
     camera.lookAt(new THREE.Vector3(0, 0, 0)); // look at origin 
-    
+
     // render
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(container.clientWidth, container.clientWidth);
     renderer.setClearColor(0xffffff, 0);
     container.appendChild(renderer.domElement); // canvas added to html here
-    
+
     // axis - xyz lines
     scene.add(new THREE.AxesHelper(3));
-    
+
     // Init a group
     this.cube = new THREE.Group();
-    
+
     // cube - mesh
     var geometry = new THREE.BoxGeometry(3, 3, 3); // 3x3x3 cube
     var material = new THREE.MeshBasicMaterial({ color: 0xffffff }); // color = black
-    
-    // cube face color
-    geometry.faces[0].color = new THREE.Color(0xF00000);
-    geometry.faces[1].color = new THREE.Color(0xF00000);
-
-    geometry.faces[2].color = new THREE.Color(0x0F0000);
-    geometry.faces[3].color = new THREE.Color(0x0F0000);
-
-    geometry.faces[4].color = new THREE.Color(0x00F000);
-    geometry.faces[5].color = new THREE.Color(0x00F000);
-
-    geometry.faces[6].color = new THREE.Color(0x000F00);
-    geometry.faces[7].color = new THREE.Color(0x000F00);
-
-    geometry.faces[8].color = new THREE.Color(0x0000F0);
-    geometry.faces[9].color = new THREE.Color(0x0000F0);
-
-    geometry.faces[10].color = new THREE.Color(0x00000F);
-    geometry.faces[11].color = new THREE.Color(0x00000F);
-    geometry.colorsNeedUpdate = true
-
     var mesh = new THREE.Mesh(geometry, material); // cube instance
-    this.cube.add(mesh); // add black cube to group
     
+    // south
+    mesh.geometry.faces[0].color.setHex(  0xffd000 );
+    mesh.geometry.faces[1].color.setHex(  0xffd000 );
+
+    // north
+    mesh.geometry.faces[2].color.setHex(  0x602ff7 );
+    mesh.geometry.faces[3].color.setHex(  0x602ff7 );
+
+    // up
+    mesh.geometry.faces[4].color.setHex(  0xff46a4 );
+    mesh.geometry.faces[5].color.setHex(  0xff46a4 );
+
+    // down
+    mesh.geometry.faces[6].color.setHex(  0xabacab );
+    mesh.geometry.faces[7].color.setHex(  0xabacab );
+
+    // west
+    mesh.geometry.faces[8].color.setHex(  0x5ac18e );
+    mesh.geometry.faces[9].color.setHex(  0x5ac18e );
+
+    // east
+    mesh.geometry.faces[10].color.setHex( 0xf9e2f0 );
+    mesh.geometry.faces[11].color.setHex( 0xf9e2f0 );
+
+    this.cube.add(mesh); // add black cube to group
+
     // edges - outline
     var edges = new THREE.EdgesGeometry(geometry); // get edges of cube
     var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xff00ff })); // color = magenta
     this.cube.add(line); // add magenta edges to group
-    
+
     scene.add(this.cube);
-    
+
     var animate = function () {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
-    };  
+    };
     animate();
   }
 
